c7ff97fe78a0edc8b80a3217d08e85a1b9877d0a
sm/fh <55134864+smfhLTD@users.noreply.github.com>
2026-03-11 10:37:26 +0200

    Header channel + cleanup

--- node/backend.js
+++ node/backend.js
@@ -3,13 +3,12 @@
 // Node returns data on index.html according to output returned from mysql.
 /*************TASKS**********/
 /*
---- Might as well take apache out of the equation and just run everything on the node... Could be an easier and more efficient solution.
-SQLi CHANNELS:
+// Might as well take apache out of the equation and just run everything on the node... Could be an easier and more efficient solution.
 * GET DONE
 * POST SQLi DONE
-* Blind SQLi 
-* Header Based injections (request.headers
-* Cookies Based injections
+* Blind SQLi DONE
+* Header Based injections (see: http.request.headers)
+* Cookie Based injections
 * Mulitipart Form data injections
 * JSON based injections
 * SOAP/XML based injections
@@ -29,7 +28,7 @@ let http = require('node:http');
 	// Could this be used for a sort of Header Pollution? Could be useful later...
 
 //typeof(request) === http.IncomingMessage 
-function queryDatabase(clientQuery)
+function queryDatabase()
 {
 	var dbConnection = null; 
 
@@ -38,21 +37,51 @@ function queryDatabase(clientQuery)
 		password: 'secret',
 		database: 'sqli_data'
 	});
-	//can't yet perform both the querying and dbConnection creation in same func, can't fish db results out of dbConn.query()'s scope... Has to be a workaround. 
+	
+
+	//can't yet perform both the querying and dbConnection creation in same func, can't fish db results out of dbConn.query()'s scope... Has to be a better workaround. 
+	// can use response.write() here to append the results, then .end() after returning. response object should be modified if node/JS passes 
+		// function parameters by REFERENCE, rather than by VALUE
 	return dbConnection;
 }
 var server = http.createServer((request, response) => 
 {
-		var clientQuery = null;
-		var dbData = null;
+
+		var clientQuery;
+		var query;
+		var headerQuery;
+		var dbConnection;
+		var dbData;
+		var userAgent;
 		
-		if(request.method == "GET") //currently only curl works, index.html doesn't have GET elements ATM (curl "http://localhost:8000/?query=user'OR'1'='1")
+		/* {
+			host: '127.0.0.1:8000',
+			'user-agent': '{VARIABLE LENGTH}',
+			...
+		*/
+		// SELECT IF(STRCMP(CURRENT_USER(),'temp@%') = 0, 'YES', 'NO');
+		// SELECT IF(STRCMP(CURRENT_USER(),"temp@%") = 0, "YES", "NO + `');
+		// HEADER LOGGING INJECTION
+			//assuming user-agent is always the 2nd header...
+		userAgent = Object.values(request.headers)[1];
+		console.log("inserting user agent into db: " + userAgent);
+		dbConnection = queryDatabase();
+		// INSERT INTO user_agents VALUES('mozilla'); SELECT IF(2>1, SLEEP(2), 'no`');`
+		headerQuery = `INSERT INTO user_agents VALUES('${userAgent}');`
+		dbConnection.query(headerQuery, function(err, results, fields)
+		{
+			if(err) {console.log("ERROR logging user-agent: " + err)}
+
+		});
+
+
+		if(request.method == "GET")
 		{
 			response.writeHead(200, {"Content-Type": "application/json"}); 
 			clientQuery = request.url.slice(8); 
 			console.log("Received query: " + clientQuery);
 			//if((dbData = queryDatabase(clientQuery)) != null) {response.end(JSON.stringify(dbData));} FOR AFTER FIXING dbConn.query() SCOPE ISSUE
-			dbData = queryDatabase(clientQuery);
+			dbData = queryDatabase();
 				query = `SELECT id,user,password FROM users WHERE user='${clientQuery}';`;
 				dbConnection = queryDatabase();
 				dbConnection.query(query, function(err, results, fields) 
